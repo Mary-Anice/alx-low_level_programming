718641d3d3ff9988691dbdc02f2e04fbdb552bd6
“Mary-anice” <mwangianice2547@gmail.com>
2023-02-19 03:45:04 -0800

    alphABET

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,14 @@
-#include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     char c;
-    
+
     for (c = 'a'; c <= 'z'; c++)
     {
         putchar(c);
@@ -13,4 +18,3 @@ int main(void)
 
     return (0);
 }
-
