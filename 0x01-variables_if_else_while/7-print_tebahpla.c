59f1129e889d2305a1af4c4e228a8b4e3a3ea5e2
“Mary-anice” <mwangianice2547@gmail.com>
2023-02-19 06:23:47 -0800

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,14 +2,14 @@
 
 int main(void)
 {
-    char letter = 'z';  // start with 'z', the last letter of the alphabet
+    char letter = 'z';  /* start with 'z', the last letter of the alphabet */
     
     while (letter >= 'a') {
-        putchar(letter);  // print the current letter
-        letter--;         // move to the previous letter
+        putchar(letter);  /* print the current letter */
+        letter--;         /* move to the previous letter */
     }
     
-    putchar('\n');        // print a new line character
+    putchar('\n');        /* print a new line character */
     
     return 0;
 }
