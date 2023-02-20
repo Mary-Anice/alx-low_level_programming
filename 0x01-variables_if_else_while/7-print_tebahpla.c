3b53d3c53db46c7321324d5529f7dc65c2ec6260
“Mary-anice” <mwangianice2547@gmail.com>
2023-02-20 07:15:57 -0800

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
-
+#include<time.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char letter = 'z';  /* start with 'z', the last letter of the alphabet */
-    
-    while (letter >= 'a') {
-        putchar(letter);  /* print the current letter */
-        letter--;         /* move to the previous letter */
-    }
-    
-    putchar('\n');        /* print a new line character */
-    
-    return 0;
+char letter = 'z';  /* start with 'z', the last letter of the alphabet */
+
+while (letter >= 'a')
+
+{
+putchar(letter);  /* print the current letter */
+letter--;         /* move to the previous letter */
+}
+putchar('\n');        /* print a new line character */
+
+return (0);
 }
