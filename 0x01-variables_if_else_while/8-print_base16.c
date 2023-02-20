be7fdecb3b38ce9fed8126215c04e1a5e42d5795
“Mary-anice” <mwangianice2547@gmail.com>
2023-02-20 07:29:21 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,24 @@
-#include <stdio.h>
-
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int i;
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
 
-    for (i = 0; i < 10; i++)
-    {
-        putchar(i + '0');
-    }
+for (i = 0; i < 6; i++)
+{
+putchar(i + 'a');
+}
 
-    for (i = 0; i < 6; i++)
-    {
-        putchar(i + 'a');
-    }
+putchar('\n');
 
-    putchar('\n');
-
-    return 0;
+return (0);
 }
