ef05cc258a7c665de8dec736e9573c4a399c3704
“Mary-anice” <mwangianice2547@gmail.com>
2023-02-20 05:04:12 -0800

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,16 @@
 #include<stdio.h>
 
 /**
- * main-Entry point
- * 
- * Return:Always 0 (Success)
+ * main -Entry point
+ * Return: Always 0(Success)
  */
-
 int main(void)
 {
-	int i;
+int i;
 
-	for(i=0;<10;i++)
-		printf("d",i);
-	printf(\n");
+for (i = 0; i < 10; i++)
+printf("%d", i);
+printf("\n");
 
-	return(0);
+return (0);
 }
