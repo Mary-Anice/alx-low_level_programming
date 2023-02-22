fda5472c469730e08ab4d2d6ea3ca88f4d0f7323
“Mary-anice” <mwangianice2547@gmail.com>
2023-02-22 06:48:45 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet in lowercase, followed by
+ * a new line.
+ */
+void print_alphabet_x10(void)
+{
+char letter = 'a';
+int i;
+
+for (i = 0; i < 10; i++)
+while (letter <= 'z')
+_putchar(letter);
+letter = 'a'; // reset letter to 'a' for next line
+_putchar('\n');
+}
