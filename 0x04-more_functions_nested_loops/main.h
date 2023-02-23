8061a2e6a9a97974093da06fe4930dc85602e407
“Mary-anice” <mwangianice2547@gmail.com>
2023-02-23 09:05:51 -0800

    Main.H

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,11 @@
-int _putchar(char c);
+#ifndef main_h
+#define main_h
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+int _putchar(char);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
@@ -11,27 +14,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-
-root@1117a48d8d29:/alx-low_level_programming/0x04-more_functions_nested_loops# cat> 0-isupper.c
-#include "main.h"
-
-/**
- * _isupper - checks if parameter is an uppercase character.
- * @c: input character.
- * Return: 1 if is an uppercase character, 0 in other case.
- */
-int _isupper(int c)
-{
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-}
-
-root@1117a48d8d29:/alx-low_level_programming/0x04-more_functions_nested_loops# git add .
-root@1117a48d8d29:/alx-low_level_programming/0x04-more_functions_nested_loops# git commit -m "Task O isupper"
-root@1117a48d8d29:/alx-low_level_programming/0x04-more_functions_nested_loops git push
+#endif
